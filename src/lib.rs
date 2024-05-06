1a6a7e3e12a2963982b7c375327f58a9856bb6a5
g4titanx <g4titan1@gmail.com>
2024-05-06 22:31:12 +0100

    removed doc comment in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,6 @@ use std::env;
 use std::error::Error;
 use std::fs;
 
-///pub struct Config {
-///pub query: String,
-///pub file_path: String,
-///pub ignore_case: bool,
-///}
-///
-///impl Config {
-///    pub fn build(
-///        mut args: impl Iterator<Item = String>
-/// ) -> Result<Config, &'static str> {
-///args.next();
-/// 
-///let query = match args.next() { //next() returns a Result type
-///Some(arg) => arg,
-///None => return Err("Didn't get a query string"),
-///};
-///
-///let file_path = match args.next() {
-///Some(arg) => arg,
-///None => return Err("Didn't get a file path"),
-/// };
-///let ignore_case = env::var("IGNORE_CASE").is_ok();
-/// Ok(Config {
-/// query,
-/// file_path,
-/// ignore_case,
-/// })
-/// }
-///}
 pub struct Config {
     pub query: String,
     pub file_path: String,
